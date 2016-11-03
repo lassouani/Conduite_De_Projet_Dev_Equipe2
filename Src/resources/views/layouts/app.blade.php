455c53b34eff4eb460c7f5d555fff67911d721cc
Lassouani <lassaouni_sofiane@outlook.com>
2016-11-03 17:34:14 +0100

    modification de welcome

--- Src/resources/views/layouts/app.blade.php
+++ Src/resources/views/layouts/app.blade.php
@@ -49,9 +49,15 @@ echo json_encode([
                         </button>
 
                         <!-- Branding Image -->
+                         @if (Auth::guest())
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            Home
+                        </a>
+                        @else
                         <a class="navbar-brand" href="{{ url('/home') }}">
                             Home
                         </a>
+                        @endif
                     </div>
 
                     <div class="collapse navbar-collapse" id="app-navbar-collapse">
@@ -137,5 +143,15 @@ echo json_encode([
 
         <!-- Scripts -->
         <script src="{{ URL::asset('js/app.js') }}"></script>
+         <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     </body>
 </html>
