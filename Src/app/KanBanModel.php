9f3bd88388156cd234c92163d818ec95c329aaec
Lassouani <lassaouni_sofiane@outlook.com>
2016-11-25 17:38:19 +0100

    requette de recuperation kanban

--- Src/app/KanBanModel.php
+++ Src/app/KanBanModel.php
@@ -16,4 +16,32 @@ class KanBanModel extends Model {
         return $this->belongsTo('App\Sprint', 'id_sprint');
     }
 
+
+public function GetKanBan($idProject,$idSprint){
+
+	 $querry = DB::table('projects')
+
+	            ->join('sprint', 'sprint.id_project', '=',    //
+                        'projects.id')
+
+                ->join('userstory', 'userstory.id_project', '=',   //
+                        'projects.id')
+
+                ->join('taches', 'taches.id_us', '=', 'userstory.id')  //
+
+                ->select('taches.description', 'taches.state',     //
+                        'taches.effort','taches.priority', 
+                            'sprint.sprint_number','sprint.id','projects.id')
+
+                //->where('sprint.id', '!=', $idSprint)  // 
+
+                ->where('userstory.id_project', '=', $idProject)
+
+                ->where('sprint.sprint_number', '=', $idSprint)
+
+                
+                ->get();
+        return $querry;
+}
+
 }
